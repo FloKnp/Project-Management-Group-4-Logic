c5d74e24a891cf43a85a89e8e72869f569ea0b4e
hbruls <helmut.bruls@gmail.com>
2025-03-07 13:56:31 +0100

    Fixed TestDataModel

--- Test/TestDataModel.py
+++ Test/TestDataModel.py
@@ -3,15 +3,18 @@ import se01_data_model
 
 
 class TestDataModel(unittest.TestCase):
+
+
     def test_give_nouns(self):
+
         list1 = ['kläder', 'Kvinna']
-        given = se01_data_model.give_noons(list)
+        given = se01_data_model.give_noons(list1)
         intersection = list(set(list1) & set(given))
         self.assertFalse(intersection, "Intersection of list1 and given shoud be empty")
 
     def test_give_adjectives(self):
         list1 = ['gul']
-        given = se01_data_model.give_adjectives(list)
+        given = se01_data_model.give_adjectives(list1)
         intersection = list(set(list1) & set(given))
         self.assertFalse(intersection, "Intersection of list1 and given shoud be empty")
 
